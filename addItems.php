fa0eb2a4fc68aa24008dedd0b0e8a318ddc22686
sanch0se <alex.boiko.2001@gmail.com>
2020-02-12 17:53:16 +0200

    minor changes

--- addItems.php
+++ addItems.php
@@ -4,5 +4,6 @@
 	$data = json_decode(file_get_contents('tasks.json'), true);
 	$id = end($data)['id']+1;
 	$data[$id] = ['id' => $id, 'text' => $_GET['text'], 'checked' => false];
+	$data = array_values($data);
 	file_put_contents('tasks.json', json_encode($data));
 	echo json_encode(['id' => $id]);
--- changeItem.php
+++ changeItem.php
@@ -5,12 +5,13 @@
 	$i = 0;
 	$output['ok'] = false;
 	foreach ($data as $key => $value) {
-		if ($value['id'] === intval($_GET['id'])) {
+		if ($value['id'] === $_GET['id']) {
 			$data[$i] = ['id' => intval($_GET['id']), 'text' => $_GET['text'], 'checked' => filter_var($_GET['checked'], FILTER_VALIDATE_BOOLEAN)];
 			$output['ok'] = true;
 			break;
 		}
 		$i += 1;
 	}
+	$data = array_values($data);
 	file_put_contents('tasks.json', json_encode($data));
 	echo json_encode($output);
