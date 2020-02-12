f1601b392f21505c5b045ea2b2ba4a1da935f943
sanch0se <alex.boiko.2001@gmail.com>
2020-02-12 23:07:20 +0200

    minor changes

--- deleteItem.php
+++ deleteItem.php
@@ -1,5 +1,5 @@
 <?php
-	require_once = 'err-handler';
+	require_once 'err-handler';
 	header("Access-Control-Allow-Origin: *");
 	header("Access-Control-Allow-Methods: GET, POST");
 		if (isset($_GET['id']) && $_GET['id'] != undefined) {
