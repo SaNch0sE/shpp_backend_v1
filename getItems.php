3d0ce389dafe7c0021231e35f0ddc5755ac83eb5
sanch0se <alex.boiko.2001@gmail.com>
2020-02-12 23:56:58 +0200

    modified:   getItems.php

--- getItems.php
+++ getItems.php
@@ -1,5 +1,5 @@
 <?php
-	require_once 'err-handler';
+	require_once 'err-handler.php';
 	header("Access-Control-Allow-Origin: *");
 	header("Access-Control-Allow-Methods: GET, POST");
 	$tasks = json_decode(file_get_contents('tasks.json'));
