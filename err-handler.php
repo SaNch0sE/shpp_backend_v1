9d9a81e8687555986ab6a163a90ea3f749941260
sanch0se <alex.boiko.2001@gmail.com>
2020-02-21 02:09:23 +0200

    modified:   err-handler.php

--- err-handler.php
+++ err-handler.php
@@ -1,7 +1,6 @@
 <?php 
     header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    header ("Access-Control-Allow-Headers: *");
+    header("Access-Control-Allow-Methods: OPTIONS, POST, GET");
 	error_reporting(E_ALL & ~E_NOTICE);
     ini_set('display_errors', 1);
     function excepHandler($exception) {
